69431c51a09884df4e388d53ea91e09499d38215
nkosinathim <nkosinathim@capitallegacy.co.za>
2026-02-05 17:59:40 +0200

    Changes made on all files

--- src/test/java/StepDefinitions/Runner.java
+++ src/test/java/StepDefinitions/Runner.java
@@ -1,14 +1,16 @@
 package StepDefinitions;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/Features",
         glue = "StepDefinitions",
         monochrome = true,
         plugin = {"pretty"}
 )
-public class Runner extends AbstractTestNGCucumberTests {
+public class Runner{
 }
 
